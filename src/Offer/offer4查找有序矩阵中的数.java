7c6778f3b68d542986e801fb9b1adcb97feb689b
liunan <737720233@qq.com>
2019-04-15 21:22:58 +0800

    剑指offer查找有序矩阵中的数

--- src/Offer/offer4查找有序矩阵中的数.java
+++ src/Offer/offer4查找有序矩阵中的数.java
@@ -1,6 +1,13 @@
 package Offer;
 
-public class FindNumberInMatrix {
+import org.junit.Test;
+
+/**
+ * 题目描述
+ 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ 剑指offer4
+ */
+public class offer4查找有序矩阵中的数 {
 
     public static void main(String[] args) {
         int[][] array = new int[][]{{}};
@@ -17,7 +24,6 @@ public class FindNumberInMatrix {
              n = array[0].length;
         } else
             return false;
-
         int i = 0;
         int j = n - 1;
         while (i < m && j >= 0) {
@@ -30,5 +36,8 @@ public class FindNumberInMatrix {
 
         return false;
     }
+
+
+
 
 }
