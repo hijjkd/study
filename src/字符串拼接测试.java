0b6bf4985e59c7f32aa45baa5e20f6ee5ad90d4f
刘楠 <liunan12@meituan.com>
2020-11-13 11:05:11 +0800

    字符串拼接耗时

--- src/字符串拼接测试.java
+++ src/字符串拼接测试.java
@@ -1,42 +1,58 @@
 public class 字符串拼接测试 {
     /**
      * 测试使用不同方式进行字符串拼接时的耗时
+     * currentTimeMillis精度不够的时候，可以使用nanoTime
      * @param args
      */
     public static void main(String[] args) {
         /**
-         * format
+         * format 占位符方式
          */
 
-        String test;
-        long sTART = System.currentTimeMillis();
-        test = String.format("%d测试format%d", 2, 3);
-        long useTime = System.currentTimeMillis() - sTART;
-        System.out.println(test + "耗时" + useTime);
+        String formatTest;
+        long startTime = System.currentTimeMillis();
+        formatTest = String.format("%S测试format%S", "A", "B");
+        long useTime = System.currentTimeMillis() - startTime;
+        System.out.println(formatTest + "耗时:" + useTime);
+        System.out.println("===================================");
 
 
-        long join = System.nanoTime();
+
+
+        /**
+         * + 拼接方式
+         */
+
+        long startTime1 = System.currentTimeMillis();
         String s1 = "2" + "拼接字符串";
-        long useTime1 = System.nanoTime() - join;
+        long useTime1 = System.currentTimeMillis() - startTime1;
         System.out.println(s1 + "耗时" + useTime1);
+        System.out.println("===================================");
 
 
+
+
+        /**
+         * StringBuilder.append() 方式
+         */
         StringBuilder stringBuilder = new StringBuilder();
         long append = System.nanoTime();
-
-        stringBuilder.append(2).append("拼接字符串");
-
+        stringBuilder.append("append方式").append("拼接字符串");
         long useTime2 = System.nanoTime() - append;
         System.out.println(stringBuilder + "耗时" + useTime2);
-
-        int i = 9;
-        System.out.println(9 + "fenz");
+        System.out.println("===================================");
 
 
+
+
+
+        /**
+         * String.value()耗时
+         */
         long v = System.currentTimeMillis();
         String.valueOf(198276);
         long m = System.currentTimeMillis() - v;
-        System.out.println("valueof耗时" + m);
+        System.out.println("String.value()耗时" + m);
 
 
     }
